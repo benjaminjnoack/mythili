cd3a1bbfdc0843675cd91f185dd080466f26fd14
Benjamin Noack <bnoack@nexient.com>
2015-10-02 11:46:09 -0400

    one of many

--- modules/Mythili.js
+++ modules/Mythili.js
@@ -1,10 +1,13 @@
-var Request = require('./Request.js'),
+var url		= require('url'),
+	Request = require('./Request.js'),
 	Options = require('./Options.js'),
 	Writer	= require('./Writer.js');
 
 var Mythili = function () {
 	this.writer = new Writer();
-	this.headerCheck = /^<([^>]+)>\; rel\=\"([\w]+)\"/;
+	this.headerCheck = /^<([^>]+)>\; rel\=\"([\w]+)\"\,\s<([^>]+)>\; rel\=\"([\w]+)\"/;
+	this.lastPage = null;
+	this.currentPage = 1;
 };
 
 Mythili.prototype.makeRequest = function(queries) {
@@ -14,7 +17,16 @@ Mythili.prototype.makeRequest = function(queries) {
 
 Mythili.prototype.handleResponse = function(resHeaders) {
 	var link = this.headerCheck.exec(resHeaders.link);
+	if (!this.lastPage) this.findLastPage(link);
+	console.log("Processed page %s of %d", this.currentPage++,  this.lastPage);
 	if (link) return (link[2] === "next") ? this.makeRequest(link[1]) : this.writer.closeStream();
+};
+
+Mythili.prototype.findLastPage = function(link) {
+	if (link[4] === "last") {
+		var parsedUrl = url.parse(link[3], true);
+		this.lastPage = parsedUrl.query.page;
+	}
 };
 
 Mythili.prototype.start = function() {
