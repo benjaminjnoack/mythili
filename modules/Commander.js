cae226a114ce7dda5f92a92ecf03f97e072f37fb
Benjamin Noack <bnoack@nexient.com>
2015-10-02 19:35:02 -0400

    [#5 #6]Added command line recognition for status and labels

--- modules/Commander.js
+++ modules/Commander.js
@@ -1,7 +1,33 @@
 var Commander = function () {
-	process.argv.forEach(function (val, index, arr){
-		console.log(index, val);
-	});
+	this.argv = process.argv;
+	this.lables = null;
+	this.status = null;
+	this.processArgv();
+};
+
+Commander.prototype.processArgv = function() {
+	for (var i = 0; i < this.argv.length; i++) {
+		var arg = this.argv[i];
+		if ((arg === '-l' || arg === '--labels') && this.argv[i + 1]) {
+			this.processLabels(i + 1);
+			continue;
+		}
+
+		if ((arg === '-s' || arg === '--status') && this.argv[i + 1]) {
+			this.processStatus(i + 1);
+			continue;
+		}
+	};
+};
+
+Commander.prototype.processLabels = function(index) {
+	var labels = this.argv[index];
+	console.log("Found labels", labels);
+};
+
+Commander.prototype.processStatus = function(index) {
+	var status = this.argv[index];
+	console.log("Found status", status);
 };
 
 module.exports = Commander;
