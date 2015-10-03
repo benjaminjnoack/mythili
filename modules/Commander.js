b73af468e7f88710f4cc7e3d6c59f9437ad9ff98
Benjamin Noack <bnoack@nexient.com>
2015-10-02 20:47:58 -0400

    [#10]added direction cmd

--- modules/Commander.js
+++ modules/Commander.js
@@ -1,5 +1,6 @@
 var Commander = function () {
 	this.argv = process.argv;
+	this.direction = null;
 	this.lables = null;
 	this.sort = null;
 	this.state = null;
@@ -9,13 +10,14 @@ Commander.prototype.processArgv = function() {
 	for (var i = 0; i < this.argv.length; i++) {
 		var arg = this.argv[i];
 		var argVal = this.argv[i + 1]
-		if ((arg === '--labels') && argVal) {
-			this.processLabels(argVal);
+		
+		if ((arg === '--direction') && argVal) {
+			this.processDirection(argVal);
 			continue;
 		}
 
-		if ((arg === '--state') && argVal) {
-			this.processState(argVal);
+		if ((arg === '--labels') && argVal) {
+			this.processLabels(argVal);
 			continue;
 		}
 
@@ -23,24 +25,35 @@ Commander.prototype.processArgv = function() {
 			this.processSort(argVal);
 			continue;
 		}
+
+		if ((arg === '--state') && argVal) {
+			this.processState(argVal);
+			continue;
+		}
 	};
+};
+
+Commander.prototype.processDirection = function(argVal) {
+	if (argVal === "asc" || argVal === "desc") this.direction = argVal;
 };
 
 Commander.prototype.processLabels = function(argVal) {
 	this.labels = argVal;
 };
 
-Commander.prototype.processState = function(argVal) {
-	if (argVal === "open" || argVal === "closed" || argVal === "all") this.state = argVal;
-};
-
 Commander.prototype.processSort = function(argVal) {
 	if (argVal === "created" || argVal === "updated" || argVal === "comments") this.sort = argVal;
 };
 
+Commander.prototype.processState = function(argVal) {
+	if (argVal === "open" || argVal === "closed" || argVal === "all") this.state = argVal;
+};
+
 Commander.prototype.getQueries = function() {
 	this.processArgv();
+	
 	return {
+		direction: this.direction,
 		labels: this.labels,
 		sort: this.sort,
 		state: this.state
