9ff2ca63d44223af19460d642fbd1b085a7c90ae
Benjamin Noack <bnoack@nexient.com>
2015-10-02 20:42:14 -0400

    [#9]cleaned up Commander and added sort argument

--- modules/Commander.js
+++ modules/Commander.js
@@ -1,37 +1,48 @@
 var Commander = function () {
 	this.argv = process.argv;
 	this.lables = null;
+	this.sort = null;
 	this.state = null;
 };
 
 Commander.prototype.processArgv = function() {
 	for (var i = 0; i < this.argv.length; i++) {
 		var arg = this.argv[i];
-		if ((arg === '--labels') && this.argv[i + 1]) {
-			this.processLabels(i + 1);
+		var argVal = this.argv[i + 1]
+		if ((arg === '--labels') && argVal) {
+			this.processLabels(argVal);
 			continue;
 		}
 
-		if ((arg === '--state') && this.argv[i + 1]) {
-			this.processState(i + 1);
+		if ((arg === '--state') && argVal) {
+			this.processState(argVal);
+			continue;
+		}
+
+		if ((arg === '--sort') && argVal) {
+			this.processSort(argVal);
 			continue;
 		}
 	};
 };
 
-Commander.prototype.processLabels = function(index) {
-	this.labels = this.argv[index];
+Commander.prototype.processLabels = function(argVal) {
+	this.labels = argVal;
 };
 
-Commander.prototype.processState = function(index) {
-	var state = this.argv[index];
-	if (state === "open" || state === "closed" || state === "all") this.state = state;
+Commander.prototype.processState = function(argVal) {
+	if (argVal === "open" || argVal === "closed" || argVal === "all") this.state = argVal;
+};
+
+Commander.prototype.processSort = function(argVal) {
+	if (argVal === "created" || argVal === "updated" || argVal === "comments") this.sort = argVal;
 };
 
 Commander.prototype.getQueries = function() {
 	this.processArgv();
 	return {
 		labels: this.labels,
+		sort: this.sort,
 		state: this.state
 	}
 };
